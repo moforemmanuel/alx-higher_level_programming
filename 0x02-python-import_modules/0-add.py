13d6d022ff25ca2d66d51c8b7961b468ba01832f
moforemmanuel <moforemmanuel1000@gmail.com>
2021-09-10 01:16:50 -0700

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-print("{} + {} = {}".format(1,2,add(1,2)))
+    print("{} + {} = {}".format(1,2,add(1,2)))
