7b90d55e5de5ca790f32da40ac94dd9f2a0abdad
moforemmanuel <moforemmanuel1000@gmail.com>
2021-11-19 07:21:23 -0800

    move_to_utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,18 +1,10 @@
--- Convert 'hbtn_0c_0' to utf8mb4, collate utf8mb4_unicode_ci
--- Convert db 'hbtn_0c_0'
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
--- Convert table 'first_table'
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert field 'name' in 'first_table'
-USE hbtn_0c_0;
-ALTER TABLE first_table
-CHANGE name name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- Convert 'hbtn_0c_0' to utf8mb4, collate utf8mb4_unicode_ci               
+-- Convert db 'hbtn_0c_0'                                                   
+USE hbtn_0c_0
+-- convert DB                                                                
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- convert first_table                                                      
+ALTER TABLE first_table	CONVERT	TO CHARACTER SET utf8mb4 COLLATE utf8mb4_un\
+icode_ci;
+-- convert name field                                                       
+ALTER TABLE first_table	MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
