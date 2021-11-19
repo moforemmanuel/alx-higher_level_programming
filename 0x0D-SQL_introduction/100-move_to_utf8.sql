7a644bf8bc21dfc689cf8011702c82c13391b025
moforemmanuel <moforemmanuel1000@gmail.com>
2021-11-19 07:23:16 -0800

    100-move_to_utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,9 @@
--- Convert 'hbtn_0c_0' to utf8mb4, collate utf8mb4_unicode_ci               
--- Convert db 'hbtn_0c_0'                                                   
-USE hbtn_0c_0
--- convert DB                                                                
+-- Change database to utf-8
+-- Use hbtn_0c_0 database
+USE hbtn_0c_0;
+-- Change name to utf-8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+-- Change table to utf-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change database to utf-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- convert first_table                                                      
-ALTER TABLE first_table	CONVERT	TO CHARACTER SET utf8mb4 COLLATE utf8mb4_un\
-icode_ci;
--- convert name field                                                       
-ALTER TABLE first_table	MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
