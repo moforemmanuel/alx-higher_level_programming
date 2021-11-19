84f1c595a443389e68fb62cd90ab22b66d5d0a4a
= <=>
2021-11-19 17:04:44 +0100

    avg_temperatures

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Import in hbtn_0c_0 database this table dump
+-- Displays cities avg temperature in accord
+
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp`;
