d85fbe28cd23c004ac8b4a5e9716ef43fd238e16
moforemmanuel <moforemmanuel1000@gmail.com>
2021-11-20 06:29:46 -0800

    avg_temperatures

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,9 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump
-mysql -u root < temperatures.sql
-
 -- Displays cities avg temperature in accord
 
 SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 GROUP BY `city`
-ORDER BY `avg_temp`;
+ORDER BY `avg_temp` DESC;
