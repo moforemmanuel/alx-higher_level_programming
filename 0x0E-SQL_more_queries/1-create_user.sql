8a0a27455c4961e6983eff141205398acc16978f
moforemmanuel <moforemmanuel1000@gmail.com>
2021-11-20 07:42:45 -0800

    create_user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,4 @@
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIViLEGES;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
