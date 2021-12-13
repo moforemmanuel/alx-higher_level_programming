fed28c1192ffab2f825d315f6e825a66218dc677
= <=>
2021-12-13 21:14:10 +0100

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const num = process.argv[2];
+const x = parseInt(num);
+
+if (isNaN(x)) {
+  console.log('Mising size');
+} else {
+  for (let i = 0; i < x; i++) {
+      let star = '';
+    for (let j = 0; j < x; j++) {
+      star += 'X';
+    }
+    console.log(star);
+  }
+}
