3d49dda3ad68f3a88673976570a294ab27aee596
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-07-12 00:01:16 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/try1.py
+++ 0x0C-python-almost_a_circle/tests/try1.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""This is a basic test"""
 
 import unittest
 
 
 class TestString(unittest.TestCase):
+    """This is a test case for strings"""
+
     def test_alnum(self):
+        """This is a test for alnum chars"""
         self.assertEqual('hello'.isalnum(), True)
 
 
