0ed4fdb4c66977e85355dbde99407df6acfe1066
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-07-12 20:13:22 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,10 +12,16 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            self.__nb_objects += 1
-            self.id = self.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
 
 if __name__ == "__main__":
     b = Base()
     print(b.id)
+    b2 = Base()
+    print(b2.id)
+    b3 = Base()
+    print(b3.id)
+    b4 = Base()
+    print(b4.id)
