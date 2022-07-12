ba6a81e33cbee9c5ff8ed03a3768c95ba439281a
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-07-12 21:09:24 +0100

    rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,26 +27,32 @@ class Rectangle(Base):
         """setter for width"""
         self.__width = width
 
-    def get_height(self, width):
+    @property
+    def height(self, width):
         """getter for height"""
         return self.__height
 
-    def set_height(self, height):
+    @height.setter
+    def height(self, height):
         """setter for height"""
         self.__height = height
 
-    def get_x(self):
+    @property
+    def x(self):
         """getter for x"""
         return self.__x
 
-    def set_x(self, x):
+    @x.setter
+    def x(self, x):
         """setter for x"""
         self.__x = x
 
-    def get_y(self, y):
+    @property
+    def y(self, y):
         """getter for y"""
         return self.__y
 
-    def set_y(self, y):
+    @y.setter
+    def y(self, y):
         """setter for y"""
         self.__y = y
