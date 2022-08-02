9a0d9b2df6cca8c99c8c0de29574c5fabeaf92f3
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-02 11:20:48 +0100

    and now the square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module implements a square object
+"""
+
+# pylint: disable=redefined-builtin
+
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class implementation of a square
+    """
+    def __init__(self, size: int, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        size getter method
+        Returns: sizeof instance side
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("height must be an integer")
+        if size <= 0:
+            raise ValueError("height must be > 0")
+        self.size = size
+        self.height = self.width = size
+
+
+if __name__ == '__main__':
+    s = Square(2, 2, 2, 2)
+    s.update(id=4302)
+    print(s)
