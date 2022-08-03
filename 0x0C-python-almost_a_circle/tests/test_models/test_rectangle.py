cbb6f9e8ef8b5e36f285272be32fa4fb6f35288f
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-03 09:42:05 +0100

    tests rect

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,10 +11,14 @@ class TestRectangle(unittest.TestCase):
     rect test model
     """
     def test_initialization(self):
-        r1 = Rectangle(2,5)
+        r1 = Rectangle(1, 2)
         self.assertEqual(r1.id, Rectangle._Base__nb_objects)
-        r2 = Rectangle(1,2)
+        r2 = Rectangle(1, 2, 3)
         self.assertEqual(r2.id, Rectangle._Base__nb_objects)
+        r3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r3.id, Rectangle._Base__nb_objects)
+        r4 = Rectangle("1", 2)
+        self.assertRaises(TypeError)
 
 if __name__ == '__main__':
     unittest.main()
