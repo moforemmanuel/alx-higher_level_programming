3599fcdea8a745112536c450a12945efe2afaa5b
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-08-03 03:44:41 +0100

    square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,107 +1,69 @@
-# #!/usr/bin/python3
-# """
-# This module implements a square object
-# """
-#
-# # pylint: disable=redefined-builtin
-#
-# from .rectangle import Rectangle
-#
-#
-# class Square(Rectangle):
-#     """
-#     Class implementation of a square
-#     """
-#     def __init__(self, size, x=0, y=0, id=None):
-#         super().__init__(size, size, x, y, id)
-#         self.__size = size
-#
-#     @property
-#     def size(self):
-#         """
-#         size getter method
-#         Returns: sizeof instance side
-#
-#         """
-#         return self.__size
-#
-#     @size.setter
-#     def size(self, size):
-#         if not isinstance(size, int):
-#             raise TypeError("width must be an integer")
-#         if size <= 0:
-#             raise ValueError("width must be > 0")
-#         self.__size = size
-#         self.width = self.height = size
-#
-#     def __str__(self):
-#         """
-#         overloading string repr
-#         Returns: instance repr
-#         """
-#         return f'[{self.__class__.__name__}] ({self.id})' \
-#                f' {self.x}/{self.y} - {self.__size}'
-#
-#
-# if __name__ == '__main__':
-#     s = Square(-1)
-#     # s.update(id=4302)
-#     # s.update(size=-1)
-#     print(s)
-
-
 #!/usr/bin/python3
 """
-This module implements a Square object
+This module implements a square object
 """
-from models.rectangle import Rectangle
+
+# pylint: disable=redefined-builtin
+# pylint: disable=unbalanced-tuple-unpacking
+
+from .rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square implementation"""
-
-    def __init__(self, size: int, x=0, y=0, id=None):
-        """initialization
-        """
+    """
+    Class implementation of a square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         self.__size = size
 
     @property
-    def size(self) -> int:
-        """size getter
+    def size(self):
+        """
+        size getter method
+        Returns: sizeof instance side
+
         """
         return self.__size
 
     @size.setter
-    def size(self, value: int):
-        """size setter
-        """
-        self.__size = value
-        self.width = self.height = value
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = size
+        self.width = self.height = size
 
-    def __str__(self) -> str:
-        """string representation"""
-        id = self.id
-        size = self.__size
-        x = self.x
-        y = self.y
-        return "[Square] ({}) {}/{} - {}".format(id, x, y, size)
+    def __str__(self):
+        """
+        overloading string repr
+        Returns: instance repr
+        """
+        return f'[{self.__class__.__name__}] ({self.id})' \
+               f' {self.x}/{self.y} - {self.__size}'
 
     def update(self, *args, **kwargs):
-        """update arguments"""
-        attr = ['id', 'size', 'x', 'y']
-        if args:
-            for at, numb in zip(attr, args):
-                setattr(self, at, numb)
-        elif kwargs:
-            for key, value in kwargs.items():
-                if key in attr:
-                    setattr(self, key, value)
+        """
+        Args:
+            *args: integers to update args
+            **kwargs: named args to update instance
 
-    def to_dictionary(self) -> dict:
-        """square to dictionary"""
-        id = self.id
-        size = self.__size
-        x = self.x
-        y = self.y
-        return {'id': id, 'x': x, 'size': size, 'y': y}
+        Returns:
+
+        """
+        values = (self.id, self.size, self.x, self.y)
+        if args:
+            self.id, self.size, self.x, self.y =\
+                args + values[len(args):]
+
+        elif kwargs:
+            for (name, value) in kwargs.items():
+                setattr(self, name, value)
+
+
+if __name__ == '__main__':
+    s = Square(-1)
+    # s.update(id=4302)
+    # s.update(size=-1)
+    print(s)
