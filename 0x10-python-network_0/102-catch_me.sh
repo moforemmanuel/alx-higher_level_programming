01341cb52802bb86e78c15f873e2b847e3a6c2ff
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-09-23 13:34:28 +0100

    102-catch_me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # catch me
-curl -s -X PUT -H "Content-Type: text/html" 0.0.0.0:5000/catch_me -F "body=You got me!"
+curl -s -X PUT -H "Content-Type: text/html; charset=utf-8" 0.0.0.0:5000/catch_me -F "body=You got me!"
