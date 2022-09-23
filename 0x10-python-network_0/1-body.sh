1ba9a25c29a577b7c26cfcb14ba113b7a448e89d
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-09-23 11:09:17 +0100

    1-body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # print body if status success
-if [ $(curl -LI "$1" -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then curl -sb "$1"; fi
+if [ $(curl -LI "$1" -o /dev/null -w '%{http_code}\n' -s) == "200" ]; then curl -s "$1"; fi
