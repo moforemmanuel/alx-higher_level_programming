499ca40f62520a3bf3351f95563c0363987acb81
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-09-23 11:51:16 +0100

    5-post_params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send post with params
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
