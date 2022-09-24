54103d64a473cf4c5f5ddc830d8e9cc4f17830b5
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-09-24 03:26:08 +0100

    header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+get req header
+"""
+
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.info()['-Request-Id'])
