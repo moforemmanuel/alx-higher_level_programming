31ad2ff222fc993e98c4c4b046ceafeed728cfb8
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 03:54:49 +0100

    starwars actor count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv.slice(2);
+
+request.get(argv[0], (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  // console.log(response.body);
+  console.log(
+    JSON.parse(body).results.reduce((acc, movie) => {
+      // console.log(movie.characters);
+      if (
+        movie.characters.includes('https://swapi-api.hbtn.io/api/people/18/')
+      ) {
+        console.log('yes');
+        console.log(acc);
+        return acc + 1;
+      }
+    }, 0)
+  );
+});
