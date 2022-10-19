506762f8c4934e470ae6ed65d413a40b2fb98d52
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 02:28:13 +0100

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,10 @@
 const fs = require('fs');
 const argv = process.argv.slice(2);
 
-fs.readFile(argv[0], function (error) {
-  if (error) console.log(error);
+fs.readFile(argv[0], 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(data);
 });
