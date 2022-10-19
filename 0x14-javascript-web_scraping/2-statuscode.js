f4df9f63a0727da59d12e4cf387d69935fd1800e
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-19 03:27:43 +0100

    statcode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,10 @@
 const request = require('request');
 const argv = process.argv.slice(2);
 
-request.get(argv[0], (error, response, body) => {
+request.get(argv[0], (error, response) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
   console.log('code: ', response && response.statusCode);
 });
